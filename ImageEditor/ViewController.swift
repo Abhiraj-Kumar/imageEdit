230e54f73a27b76ec5cc02fb550391c0c8ccc351
Abhiraj Kumar <abhiraj@pt-mac-4.local>
2015-12-05 12:55:06 +0530

    Initial Commit

---
+++ ImageEditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageEditor
+//
+//  Created by proptiger on 05/12/15.
+//  Copyright © 2015 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
